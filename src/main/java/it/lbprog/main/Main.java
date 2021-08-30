c4849f7a33f629f66317ed96acd8da8539fc73f7
leon972 <leonardo.berti@ymail.com>
2021-08-30 22:54:18 +0200

    ddd

---
+++ src/main/java/it/lbprog/main/Main.java
@@ -0,0 +1,11 @@
+package it.lbprog.main;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("-******************hhhh****ss***----");
+        System.out.println("Matlab json exporter");
+    }
+
+}
---
+++ src/test/java/it/lbprog/matlbajsontests/Tests.java
@@ -0,0 +1,13 @@
+package it.lbprog.matlbajsontests;
+
+import org.junit.Test;
+
+public class Tests {
+
+    @Test
+    public void testMain()
+    {
+        System.out.println("Main test");
+    }
+
+}
